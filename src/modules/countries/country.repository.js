a24d97f7b96a30427e9ee312134d4216d88b2eee
Nickomo <bezzubovandrej@gmail.com>
2021-03-12 02:45:57 +0200

    Initial

--- src/modules/countries/country.repository.js
+++ src/modules/countries/country.repository.js
@@ -2,7 +2,7 @@ const Country = require('./country.schema');
 const { NotFoundError } = require('../../common/errors/errors-list');
 const { ENTITY_NAME } = require('./constants');
 const {
-  COLLECTION_NAME: PLACE_COLLECTION_NAME,
+    COLLECTION_NAME: PLACE_COLLECTION_NAME,
 } = require('../places/constants');
 const { Types } = require('mongoose');
 
@@ -10,51 +10,64 @@ const countryExcludedFields = { _id: 0, __v: 0, lang: 0, localizations: 0 };
 const placeExcludedFields = { _id: 0, countryId: 0, lang: 0, localizations: 0 };
 
 const getAllByLang = async (lang) => {
-  return await Country.aggregate()
-    .match({ localizations: { $elemMatch: { lang } } })
-    .unwind('localizations')
-    .match({ 'localizations.lang': lang })
-    .replaceRoot({
-      $mergeObjects: [{ id: '$_id' }, '$localizations', '$$ROOT'],
-    })
-    .project(countryExcludedFields);
+
+
+    Country.aggregate()
+        .match({ localizations: { $elemMatch: { lang } } })
+        .unwind('localizations')
+        .match({ 'localizations.lang': lang })
+        .replaceRoot({
+            $mergeObjects: [{ id: '$_id'}, '$localizations', '$$ROOT', ],
+        })
+        .addFields({currency: {fullName: "$localizations.currency.fullName"}})
+        .project(countryExcludedFields)
+        .then(result => console.log(result));
+    return await Country.aggregate()
+        .match({ localizations: { $elemMatch: { lang } } })
+        .unwind('localizations')
+        .match({ 'localizations.lang': lang })
+        .addFields({currency: {fullName: "$localizations.currency.fullName"}})
+        .replaceRoot({
+            $mergeObjects: [{ id: '$_id' }, '$localizations', '$$ROOT'],
+        })
+        .project(countryExcludedFields);
 };
 
 const getOneByLang = async (id, lang) => {
-  const data = await Country.aggregate()
-    .match({ _id: Types.ObjectId(id) })
-    .unwind('localizations')
-    .match({ 'localizations.lang': lang })
-    .replaceRoot({
-      $mergeObjects: [{ id: '$_id' }, '$localizations', '$$ROOT'],
-    })
-    .project(countryExcludedFields)
-    .lookup({
-      from: PLACE_COLLECTION_NAME,
-      pipeline: [
-        {
-          $match: { countryId: Types.ObjectId(id) },
-        },
-        { $unwind: '$localizations' },
-        {
-          $match: { 'localizations.lang': lang },
-        },
-        {
-          $replaceWith: { $mergeObjects: ['$localizations', '$$ROOT'] },
-        },
-        { $project: placeExcludedFields },
-      ],
-      as: 'places',
-    });
+    const data = await Country.aggregate()
+        .match({ _id: Types.ObjectId(id) })
+        .unwind('localizations')
+        .match({ 'localizations.lang': lang })
+        .replaceRoot({
+            $mergeObjects: [{ id: '$_id' }, '$localizations', '$$ROOT'],
+        })
+        .project(countryExcludedFields)
+        .lookup({
+        from: PLACE_COLLECTION_NAME,
+        pipeline: [
+            {
+            $match: { countryId: Types.ObjectId(id) },
+            },
+            { $unwind: '$localizations' },
+            {
+            $match: { 'localizations.lang': lang },
+            },
+            {
+            $replaceWith: { $mergeObjects: ['$localizations', '$$ROOT'] },
+            },
+            { $project: placeExcludedFields },
+        ],
+        as: 'places',
+        });
 
-  const country = data[0];
-  if (country) {
-    return country;
-  }
-  throw new NotFoundError(ENTITY_NAME);
+    const country = data[0];
+    if (country) {
+        return country;
+    }
+    throw new NotFoundError(ENTITY_NAME);
 };
 
 module.exports = {
-  getAllByLang,
-  getOneByLang,
+    getAllByLang,
+    getOneByLang,
 };
--- src/modules/countries/country.schema.js
+++ src/modules/countries/country.schema.js
@@ -1,50 +1,56 @@
 const { Schema, model } = require('mongoose');
 
 const localeSchema = new Schema({
-  _id: false,
-  lang: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  capital: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
+    _id: false,
+    lang: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    capital: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
 });
 
 const countrySchema = new Schema({
-  imageUrl: String,
-  videoUrl: String,
-  currency: {
-    type: String,
-    required: true,
-  },
-  ISOCode: {
-    type: String,
-    uppercase: true,
-    unique: true,
-    required: true,
-  },
-  capitalLocation: {
-    type: {
-      type: String,
-      enum: ['Point'],
-      required: true,
+    imageUrl: String,
+    videoUrl: String,
+    currency: [{
+        name: {
+            type: String,
+            required: true,
+        },
+        sign: {
+            type: String,
+            required: true,
+        },
+    }],
+    ISOCode: {
+        type: String,
+        uppercase: true,
+        unique: true,
+        required: true,
     },
-    coordinates: {
-      type: [Number],
-      required: true,
+    capitalLocation: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true,
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+        },
     },
-  },
-  localizations: [localeSchema],
+    localizations: [localeSchema],
 });
 
 const Country = model('Country', countrySchema);
